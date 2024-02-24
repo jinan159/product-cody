e3fcc9b83a7af50ff5661f4ba82bdf49d4e77765
jinan159 <wlsdhks0423@naver.com>
2024-02-24 13:49:52 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring")
+    kotlin("plugin.serialization")
+
+    application
+}
+
+group = "com.musinsa.recommand"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+
+    // exposed
+    val exposedVersion: String by project
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
+    runtimeOnly("com.h2database:h2")
+    testRuntimeOnly("com.h2database:h2")
+
+    // test
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // spring-test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // kotest
+    val kotestVersion: String by project
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+
+    // mocking
+    testImplementation("io.mockk:mockk:1.13.7")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
+    // api-documentation
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.18.0")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.compileKotlin {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.jar {
+    enabled = false
+}
+
+application {
+    mainClass.set("com.musinsa.recommand.ApplicationKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.serialization" -> useVersion(kotlinVersion)
+            }
+        }
+    }
+}
+
+rootProject.name = "product-recommand"
+
+
