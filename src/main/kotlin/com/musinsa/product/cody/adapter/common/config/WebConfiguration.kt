de3411968685830678e4b6a59477f34e2bf644bd
jinan159 <wlsdhks0423@naver.com>
2024-03-02 22:00:29 +0900

    cors 설정 추가

--- src/main/kotlin/com/musinsa/product/cody/adapter/common/config/WebConfiguration.kt
+++ src/main/kotlin/com/musinsa/product/cody/adapter/common/config/WebConfiguration.kt
@@ -1,13 +1,20 @@
 package com.musinsa.product.cody.adapter.common.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class StaticResourceConfiguration : WebMvcConfigurer {
+class WebConfiguration : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/")
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/")
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "DELETE")
     }
 }
